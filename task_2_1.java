c798e2613dbb7219314d6dd901ea6fa152471baa
Artem_notebook <mobail_2003@bk.ru>
2022-12-30 15:07:57 +0700

    add task_2_2 v.4

--- task_2_1.java
+++ task_2_1.java
@@ -15,18 +15,14 @@ public class task_2_1 {
             for (int i = 0; i < mas.length-1; i++) {
                 if(mas[i] > mas[i+1]){
                     isSorted = false;
- 
                     buf = mas[i];
                     mas[i] = mas[i+1];
                     mas[i+1] = buf;
-                    //System.out.println(Arrays.toString(mas));
-
                     Logger logger = Logger.getLogger(task_2_1.class.getName());
                     FileHandler fh = new FileHandler("log.xml");
                     logger.addHandler(fh);
                     XMLFormatter xml = new XMLFormatter();
                     fh.setFormatter(xml);
-                    //logger.log(Level.WARNING, "Test W loggin");
                     logger.info(Arrays.toString(mas));
                 }
             }
@@ -35,5 +31,3 @@ public class task_2_1 {
     }
         
 }
-
-// C:\Users\mobai\OneDrive\Документы\Soft_Dev\java_hw\
