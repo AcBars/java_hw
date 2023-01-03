d9bfbca8ec56c486230e3f91b843ab499cda6e26
Artem_notebook <mobail_2003@bk.ru>
2023-01-03 21:09:10 +0700

    add task_3_2 v.11

--- task_3_2.java
+++ task_3_2.java
@@ -1,5 +1,26 @@
 // Пусть дан произвольный список целых чисел, удалить из него четные числа
 
 public class task_3_2 {
-    
+    public static void main (String [] args) {
+        int[] sortArr = {12, 6, 4, 1, 15, 10};
+        int temp = 0;
+        for(int i = 0; sortArr.length < i; i++) {
+            if (sortArr[i] % 2 != 0) {
+                temp += 1;
+            }
+        if (temp == 0) {
+            System.out.println("null");
+        }
+        else {
+            int [] result = new int [temp];
+            for (i =0; sortArr.length < i; i++) {
+                if (sortArr[i] % 2 == 0) {
+                    
+                }
+            }
+        }
+            
+        }
+        System.out.println(Arrays.toString(result));
+    } 
 }
