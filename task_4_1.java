5c8a17f4cb9bd2428611e59001fb752365463a91
Artem_notebook <mobail_2003@bk.ru>
2023-01-12 12:23:12 +0700

    add task_4_1 v.13

--- task_4_1.java
+++ task_4_1.java
@@ -2,5 +2,57 @@
 Реализуйте метод, который вернет “перевернутый” список. */
 
 public class task_4_1 {
-    
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.push(1);
+        list.push(2);
+        list.push(3);
+        list.push(4);
+
+        System.out.println("Original List: ");
+        list.printList();
+
+        LinkedList reversedList = list.reverse();
+        System.out.println("\nReversed List: ");
+        reversedList.printList();
+    }
 }
+
+class LinkedList {
+    static class Node {
+        int data;
+        Node next;
+
+        public Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    Node head;
+
+    public void push(int data) {
+        Node newNode = new Node(data);
+        newNode.next = head;
+        head = newNode;
+    }
+
+    public LinkedList reverse() {
+        LinkedList reversedList = new LinkedList();
+        Node current = head;
+        while (current != null) {
+            reversedList.push(current.data);
+            current = current.next;
+        }
+        return reversedList;
+    }
+
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
+
